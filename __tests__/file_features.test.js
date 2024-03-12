354eee4cea84182cb4b47c80a954313bd62335df
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2024-03-12 19:17:53 -0400

    test: test several upload file types

--- __tests__/file_features.test.js
+++ __tests__/file_features.test.js
@@ -16,9 +16,9 @@ afterEach(() => {
 });
 
 /**
- * Test a file upload
+ * Test a txt file upload
  */
-describe("/uploadFile", () => {
+describe("/uploadFile txt", () => {
   async function getAuthSession() {
     const username = process.env.TEST_LOGIN_USERNAME;
     const password = process.env.TEST_LOGIN_PASSWORD;
@@ -38,6 +38,126 @@ describe("/uploadFile", () => {
       .field("Content-Type", "multipart/form-data")
       .field("eventName", eventName)
       .attach("uploadedFile", `${__dirname}/__tests__/test_file.txt`);
+    expect(res2.body.message).toBe("Upload OK");
+    expect([200]).toContain(res2.statusCode);
+
+    await server.close();
+  });
+});
+
+/**
+ * Test a pdf file upload
+ */
+describe("/uploadFile pdf", () => {
+  async function getAuthSession() {
+    const username = process.env.TEST_LOGIN_USERNAME;
+    const password = process.env.TEST_LOGIN_PASSWORD;
+    const res = await loginSession
+      .post("/login")
+      .send({ email: username, password: password });
+    expect([200, 302]).toContain(res.statusCode);
+    return loginSession;
+  }
+  it("upload a file", async () => {
+    const authSession = await getAuthSession();
+    const eventName = process.env.TEST_EVENT_NAME;
+
+    //do the file upload
+    const res2 = await authSession
+      .post("/uploadFile")
+      .field("Content-Type", "multipart/form-data")
+      .field("eventName", eventName)
+      .attach("uploadedFile", `${__dirname}/__tests__/test_file.pdf`);
+    expect(res2.body.message).toBe("Upload OK");
+    expect([200]).toContain(res2.statusCode);
+
+    await server.close();
+  });
+});
+
+/**
+ * Test a pptx file upload
+ */
+describe("/uploadFile pptx", () => {
+  async function getAuthSession() {
+    const username = process.env.TEST_LOGIN_USERNAME;
+    const password = process.env.TEST_LOGIN_PASSWORD;
+    const res = await loginSession
+      .post("/login")
+      .send({ email: username, password: password });
+    expect([200, 302]).toContain(res.statusCode);
+    return loginSession;
+  }
+  it("upload a file", async () => {
+    const authSession = await getAuthSession();
+    const eventName = process.env.TEST_EVENT_NAME;
+
+    //do the file upload
+    const res2 = await authSession
+      .post("/uploadFile")
+      .field("Content-Type", "multipart/form-data")
+      .field("eventName", eventName)
+      .attach("uploadedFile", `${__dirname}/__tests__/test_file.pptx`);
+    expect(res2.body.message).toBe("Upload OK");
+    expect([200]).toContain(res2.statusCode);
+
+    await server.close();
+  });
+});
+
+/**
+ * Test a xlsx file upload
+ */
+describe("/uploadFile xlsx", () => {
+  async function getAuthSession() {
+    const username = process.env.TEST_LOGIN_USERNAME;
+    const password = process.env.TEST_LOGIN_PASSWORD;
+    const res = await loginSession
+      .post("/login")
+      .send({ email: username, password: password });
+    expect([200, 302]).toContain(res.statusCode);
+    return loginSession;
+  }
+  it("upload a file", async () => {
+    const authSession = await getAuthSession();
+    const eventName = process.env.TEST_EVENT_NAME;
+
+    //do the file upload
+    const res2 = await authSession
+      .post("/uploadFile")
+      .field("Content-Type", "multipart/form-data")
+      .field("eventName", eventName)
+      .attach("uploadedFile", `${__dirname}/__tests__/test_file.xlsx`);
+    expect(res2.body.message).toBe("Upload OK");
+    expect([200]).toContain(res2.statusCode);
+
+    await server.close();
+  });
+});
+
+/**
+ * Test a zip file upload
+ */
+describe("/uploadFile zip", () => {
+  async function getAuthSession() {
+    const username = process.env.TEST_LOGIN_USERNAME;
+    const password = process.env.TEST_LOGIN_PASSWORD;
+    const res = await loginSession
+      .post("/login")
+      .send({ email: username, password: password });
+    expect([200, 302]).toContain(res.statusCode);
+    return loginSession;
+  }
+  it("upload a file", async () => {
+    const authSession = await getAuthSession();
+    const eventName = process.env.TEST_EVENT_NAME;
+
+    //do the file upload
+    const res2 = await authSession
+      .post("/uploadFile")
+      .field("Content-Type", "multipart/form-data")
+      .field("eventName", eventName)
+      .attach("uploadedFile", `${__dirname}/__tests__/test_file.zip`);
     expect(res2.body.message).toBe("Upload OK");
     expect([200]).toContain(res2.statusCode);
 

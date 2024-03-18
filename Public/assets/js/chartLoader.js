139cc0a255f89ceede503ba0277fe5e99b08b506
EmilioEspada <istemilioespada@gmail.com>
2024-03-18 14:02:03 -0400

    feedback reports downloadable as a csv file

--- Public/assets/js/chartLoader.js
+++ Public/assets/js/chartLoader.js
@@ -27,6 +27,12 @@ document.addEventListener("DOMContentLoaded", async () => {
       createDoughnut(insChart, dbEventInfo.Inspiring);
       createDoughnut(intChart, dbEventInfo.Interactive);
       createDoughnut(relChart, dbEventInfo.Relevant);
+
+      // Event listener attached to button to download CSV report
+      document.getElementById("downloadReportButton").addEventListener("click", () => {
+        downloadCSV(dbEventInfo);
+      });
+
     })
     .catch((error) => {
       console.error(error);
@@ -59,4 +65,32 @@ function createDoughnut(canvasElement, data) {
       cutoutPercentage: 50, //idk if this is doing anything
     },
   });
+}
+
+// functions used for downloadable report
+function downloadCSV(data){
+  // Prepare CSV content
+  let csvContent = "Category,Hate,Sad,Ok,Liked,Loved\n"
+  csvContent += prepareCSVRow("Actionable", data.Actionable);
+  csvContent += prepareCSVRow("Engaging", data.Engaging);
+  csvContent += prepareCSVRow("Inspiring", data.Inspiring);
+  csvContent += prepareCSVRow("Interactive", data.Interactive);
+  csvContent += prepareCSVRow("Relevant", data.Relevant);
+
+  // Create a Blob containing the CSV data
+  const blob = new Blob([csvContent], {
+    type: "text/csv",
+  });
+
+  // Create a link element and trigger a download
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "exported_data.csv";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function prepareCSVRow(label, data) {
+  return `${label},${Object.values(data).join(',')}\n`;
 }

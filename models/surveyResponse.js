e993512ae3efa2a3b697209fea46ff025e4eb0d1
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2024-03-30 16:48:48 -0400

    feat: survey class

---
+++ models/surveyResponse.js
@@ -0,0 +1,11 @@
+export class SurveyResponse {
+  constructor() {
+    this.actionableAns = "";
+    this.engagingAns = "";
+    this.interactiveAns = "";
+    this.inspiringAns = "";
+    this.relevantAns = "";
+    this.areasEnjoyedAns = "";
+    this.qualitiesImprovedAns = "";
+  }
+}

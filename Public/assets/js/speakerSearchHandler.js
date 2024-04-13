ad4f06fd82d1270c4d8ff8641ac5d52f8f9c7c4c
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2024-04-13 14:49:24 -0400

    refactor: convert search cards to HTML tempalte

--- Public/assets/js/speakerSearchHandler.js
+++ Public/assets/js/speakerSearchHandler.js
@@ -2,13 +2,6 @@ const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 const searchResults = document.getElementById("searchResults");
 
-/** A function to build DOM elements */
-function buildElement(type, className) {
-  const e = document.createElement(type);
-  e.className = className;
-  return e;
-}
-
 /** Check if the user record has a valid image src string */
 function checkForValidProfileImage(user) {
   return user?.profile?.profilePictureUrl === ""
@@ -16,49 +9,9 @@ function checkForValidProfileImage(user) {
     : user?.profile?.profilePictureUrl;
 }
 
-/** Build a Bootstrap card element for the user record */
-function UserCard() {
-  const userCard = buildElement("div", "card mb-3 mx-auto");
-  userCard.style.maxWidth = "540px";
-  userCard.style.cursor = "pointer";
-  return userCard;
-}
-
-/** Build a column element for a user card */
-function ImageColumn() {
-  const imageColumn = buildElement(
-    "div",
-    "col-md-4 d-flex justify-content-center align-items-center",
-  );
-  imageColumn.style.height = "100%";
-  return imageColumn;
-}
-
-/** Build an image element for a user card */
-function ProfilePicture(imageSrc) {
-  const profilePicture = buildElement("img", "img-fluid rounded-start");
-  profilePicture.src = imageSrc;
-  profilePicture.alt = "Profile Picture";
-  return profilePicture;
-}
-
 /** Clear the search results list */
 function clearSearchResults() {
   searchResults.innerHTML = "";
-}
-
-/** Function to return a display name header */
-function DisplayName(user) {
-  const e = buildElement("h5", "card-title h1");
-  e.textContent = user?.displayName;
-  return e;
-}
-
-/** Function to return a full name label */
-function FullName(user) {
-  const e = buildElement("p", "card-text lead");
-  e.textContent = `${user?.profile.firstName} ${user?.profile.lastName}`;
-  return e;
 }
 
 /** Display a message to the user if no results found */
@@ -73,6 +26,20 @@ function handleSearchError(error) {
   const err = buildElement("p", "lead");
   err.textContent = `Error searching: ${error}`;
   searchResults.appendChild(err);
+}
+
+/** Get the template element representing the card,
+ *  clone it, and populate the data
+ */
+function getHtmlTemplate(user) {
+  const image = checkForValidProfileImage(user);
+  const template = document.getElementsByTagName("template")[0];
+  const card = template.content.cloneNode(true);
+  card.querySelector("h5").textContent =
+    `${user?.profile.firstName} ${user?.profile.lastName}`;
+  card.querySelector("p").textContent = user?.displayName;
+  card.querySelector("img").src = image;
+  return card;
 }
 
 searchForm.addEventListener("submit", async (event) => {
@@ -94,29 +61,12 @@ searchForm.addEventListener("submit", async (event) => {
 
     //otherwise, build the user profile cards
     data.forEach((user) => {
-      const image = checkForValidProfileImage(user);
-      const userCard = UserCard();
-      const row = buildElement("div", "row g-0");
-      const imageColumn = ImageColumn();
-      imageColumn.appendChild(ProfilePicture(image));
-      row.appendChild(imageColumn);
-
-      const bodyColumn = buildElement(
-        "div",
-        "col-md-8 justify-content-center align-items-center",
-      );
-      const cardBody = buildElement("div", "card-body");
-
-      cardBody.appendChild(DisplayName(user));
-      cardBody.appendChild(FullName(user));
+      const userCard = getHtmlTemplate(user);
 
       // Event listener redirects to profile page when card is clicked
       userCard.addEventListener("click", () => {
         window.location.href = `/speakerProfile/${user.uid}`;
       });
-      bodyColumn.appendChild(cardBody);
-      row.appendChild(bodyColumn);
-      userCard.appendChild(row);
       searchResults.appendChild(userCard);
     });
   } catch (error) {

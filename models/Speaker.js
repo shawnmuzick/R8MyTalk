d79b4916d33886cc1edf4dbbe2446bbe42c6dac1
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2024-04-12 23:38:59 -0400

    fix: Speaker class structure

--- models/Speaker.js
+++ models/Speaker.js
@@ -2,14 +2,15 @@ export class Speaker {
   constructor(data) {
     this.uid = data?.uid;
     this.displayName = data?.displayName;
+    this.profile = {};
   }
   populateProfile(data) {
-    this.firstName = data?.firstName ?? "";
-    this.lastName = data?.lastName ?? "";
-    this.bio = data?.Bio ?? "";
-    this.socialLink1 = data?.socialLink1 ?? "";
-    this.socialLink2 = data?.socialLink2 ?? "";
-    this.socialLink3 = data?.socialLink3 ?? "";
-    this.profilePictureUrl = data?.profilePictureUrl ?? "";
+    this.profile.firstName = data?.firstName ?? "";
+    this.profile.lastName = data?.lastName ?? "";
+    this.profile.bio = data?.Bio ?? "";
+    this.profile.socialLink1 = data?.socialLink1 ?? "";
+    this.profile.socialLink2 = data?.socialLink2 ?? "";
+    this.profile.socialLink3 = data?.socialLink3 ?? "";
+    this.profile.profilePictureUrl = data?.profilePictureUrl ?? "";
   }
 }

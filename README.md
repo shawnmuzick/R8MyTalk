9dd79cb8df2389a0a8e6276e2c7060b517a3a4da
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2024-04-29 20:46:55 -0400

    chore: update docs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CS510Project
 
 ## About
+
 This project is for Parents2Partners, developed by students of CCSU. This project is intended to be iterated on through multiple classes of student developers. This repo is a fork of the previous development team. Their upstream code base can be found here: https://github.com/Gulloje/R8MyTalkGulloje/R8MyTalk
 
 If you are a future developer of this project, please fork this repo and continue development. There is no need to submit your changes upstream to this repo, though feel free to reach out to us with questions about the project. The previous developer provided us with a word document user manual that we will share with you.
@@ -9,31 +10,42 @@ The production application can be viewed here: https://r8temytalk-nkqs.onrender.
 It is using a free hosting plan, and is normal for it to be slow to start up
 
 ## Prerequisites
+
 - install nodejs LTS v20 for your platform via https://nodejs.org/en/download/
 
 ## Build a dev environment
+
 Follow these steps to set up a local development environment:
+
 - create a folder that you want to use as your project directory
 - cd to your project directory in a terminal
 - clone the repo
+
 ```
 git clone https://github.com/shawnmuzick/R8MyTalk.git
 ```
+
 - dump the env file into the root of the cloned project, on the same level as this readme. Make sure the file is named ".env", if it is not.
 - run the command to install the packages for the project:
+
 ```javascript
 npm install
 ```
+
 - when the dependencies are finished installing, you can invoke node from a terminal and call the main application entry point:
+
 ```javascript
-node index.js  
+node index.js
 ```
+
 - the application will run on port 3000, by default. View the running application in your browser at localhost:3000
 
 ## Contributing
 
 ### Branching Strategy
+
 We will follow a conventional branching strategy:
+
 ```
 main
 |
@@ -45,12 +57,15 @@ main
 |
 - hotfix
 ```
-When implementing a new feature, please create a branch from dev named as "feat-{nameOfFeature}". When the feature is complete, please submit a pull request into dev. The code will be reviewed and merged pending approval. 
+
+When implementing a new feature, please create a branch from dev named as "feat-{nameOfFeature}". When the feature is complete, please submit a pull request into dev. The code will be reviewed and merged pending approval.
 
 Main will be linked with the production environment.
 
 ### Commit Messages
+
 Please use conventional message prefixes:
+
 ```
 feat		– new feature
 fix			– bug fix
@@ -59,20 +74,3 @@ refactor	– refactoring code
 test 		– create new tests or fix existing ones
 revert 		– revert some previous commit
 ```
-
-## Todo
-- check for breaking changes in package "open" from v9 to v10
-- Fix download functionality
-- Upload a new logo that their team is designing 
-- Convert the black background into white with black text for ease of navigation 
-- Create a method to generate a downloadable report of attendee feedback 
-- Allow for the speaker to create a profile that includes a photograph and adding social media 
-- Allow the app to generate a list of emails, saved for each event. 
-
-## Backlog priority
-- file upload/download is top priority
-- download leads/email list
-- downloadable feedback report 
-- next is speaker profile page
-- website
-- low priority is logo/colors
--- util.js
+++ util.js
@@ -1,38 +1,37 @@
-import {
-  arrayUnion,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  updateDoc,
-} from "firebase/firestore";
-import {
-  deleteObject,
-  getDownloadURL,
-  ref,
-  uploadBytes,
-} from "firebase/storage";
+import { arrayUnion, collection, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
+import { deleteObject, getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import qr from "qrcode";
 import { getEventRef } from "./controllers/events.js";
 import { db, storage } from "./index.js";
 import { EmojiQuestions } from "./models/EmojiQuestions.js";
 
+/** replace spaces with "-" in a string, return the string
+ * @param string string
+ * @returns string
+ */
 export function spaceToHyphen(string) {
   return string.replace(/ /g, "-");
 }
 
+/** replace "-" with a space in a string, return the string
+ * @param string string
+ * @returns string
+ */
 export function hyphenToSpace(string) {
   return string ? string.replace(/-/g, " ") : "";
 }
 
+/** create a QR code for a given event
+ * @param {string} url - the url of the site
+ * @param {string} userFolder - the firebase user id
+ * @param {string} fileName - the name of the qr code file to store
+ * @param {string} eventName - the name of the event the qr code points to
+ */
 export async function createQR(url, userFolder, fileName, eventName) {
   try {
     // Generate the QR code
     const qrCodeData = await qr.toDataURL(url);
-    const qrCodeBuffer = Buffer.from(
-      qrCodeData.replace(/^data:image\/png;base64,/, ""),
-      "base64",
-    );
+    const qrCodeBuffer = Buffer.from(qrCodeData.replace(/^data:image\/png;base64,/, ""), "base64");
     const storageRef = ref(storage, `${userFolder}/${eventName}/${fileName}`);
 
     const metadata = {
@@ -47,15 +46,18 @@ export async function createQR(url, userFolder, fileName, eventName) {
   }
 }
 
+/** Upload a file to Firebase storage
+ * @param {blob} file - the file to upload to storage
+ * @param {string} uid - the Firebase user id
+ * @param {string} eventName - the name of the associated event
+ * @returns Firebase result
+ */
 export async function uploadSharedFiles(file, uid, eventName) {
   try {
     const metadata = {
       contentType: file.mimetype,
     };
-    const storageRef = ref(
-      storage,
-      `${uid}/${spaceToHyphen(eventName)}/${file.fieldname}`,
-    );
+    const storageRef = ref(storage, `${uid}/${spaceToHyphen(eventName)}/${file.fieldname}`);
     const result = await uploadBytes(storageRef, file.buffer, metadata);
     return result;
   } catch (error) {
@@ -63,18 +65,12 @@ export async function uploadSharedFiles(file, uid, eventName) {
     throw error;
   }
 }
-export async function deleteEventFromStorage(eventName, uid) {
-  try {
-    const fileRef = ref(storage, `${uid}/${eventName}/uploadedFile`);
-    const qrRef = ref(storage, `${uid}/${eventName}/${eventName}.png`);
-    await deleteObject(qrRef);
-    await deleteObject(fileRef);
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
 
+/** Get a download url for an event's associated uploadFile
+ * @param {string} userFolder - the firbase user id
+ * @param {string} eventName - the name of the associated event
+ * @returns  string - url of the download
+ */
 export async function getFileDownloadURL(userFolder, eventName) {
   try {
     const storageRef = ref(storage, `${userFolder}/${eventName}/uploadedFile`);
@@ -86,13 +82,15 @@ export async function getFileDownloadURL(userFolder, eventName) {
   }
 }
 
+/** Get a download url for an event's associated QR code
+ * @param {string} userFolder - the firbase user id
+ * @param {string} eventNameParam - the name of the associated event
+ * @returns  string - url of the download
+ */
 export async function getQRURL(userFolder, eventNameParam) {
   try {
     const eventName = spaceToHyphen(eventNameParam);
-    const storageRef = ref(
-      storage,
-      `${userFolder}/${eventName}/${eventName}.png`,
-    );
+    const storageRef = ref(storage, `${userFolder}/${eventName}/${eventName}.png`);
     const url = await getDownloadURL(storageRef);
     return url;
   } catch (error) {
@@ -100,6 +98,11 @@ export async function getQRURL(userFolder, eventNameParam) {
   }
 }
 
+/** Get the data for an event from the Firebase database
+ * @param {string} uid - the Firebase user id
+ * @param {string} eventName - the name of the associated event
+ * @returns Firebase document data
+ */
 export async function readEventInfoFromDB(uid, eventName) {
   try {
     const eventRef = await getEventRef(uid, eventName);
@@ -115,6 +118,13 @@ export async function readEventInfoFromDB(uid, eventName) {
   }
 }
 
+/** Get the contact data for all of user's events from the Firebase database
+ * @param {string} uid - the Firebase user id
+ * @returns array of {
+            eventName: theEventName,
+            eventData: emailFields,
+          }
+ */
 export async function readContactInfoFromDb(uid) {
   // we're going to add the event Name & any contact info from each event
   const contactsArray = [];
@@ -133,9 +143,7 @@ export async function readContactInfoFromDb(uid) {
 
         if (aContactDoc.exists()) {
           const emailFields = Object.entries(aContactDoc.data())
-            .filter(
-              ([key, value]) => typeof key === "string" && key.includes("@"),
-            )
+            .filter(([key, value]) => typeof key === "string" && key.includes("@"))
             .reduce((acc, [key, value]) => {
               acc[key] = value;
               return acc;
@@ -145,7 +153,7 @@ export async function readContactInfoFromDb(uid) {
             eventData: emailFields,
           });
         }
-      }),
+      })
     );
   } catch (error) {
     console.error("ERROR on sub-reference: ", error);
@@ -153,14 +161,15 @@ export async function readContactInfoFromDb(uid) {
   return contactsArray;
 }
 
-export async function sendContactInfoToDB(
-  fullName,
-  phoneNumber,
-  email,
-  role,
-  uid,
-  eventName,
-) {
+/** Store info received from survey contact forms in the Firebase database
+ * @param {string} fullName - the user provided name
+ * @param {string} phoneNumber - the user provided phone number
+ * @param {string} email - the user provided email
+ * @param {string} role - the user provided role string
+ * @param {string} uid - the Firebase user id
+ * @param {string} eventName -
+ */
+export async function sendContactInfoToDB(fullName, phoneNumber, email, role, uid, eventName) {
   try {
     const eventRef = await getEventRef(uid, eventName);
     const eventDoc = await getDoc(eventRef);
@@ -176,6 +185,11 @@ export async function sendContactInfoToDB(
   }
 }
 
+/** Update the question data for a particular question response on an event
+ * @param {string} question - the name/label of the question
+ * @param {string} answer - the user provided question answer
+ * @param {Object} eventRef - a Firebase doc ref object
+ */
 export async function handleQuestion(question, answer, eventRef) {
   try {
     await updateDoc(eventRef, { [question]: arrayUnion(answer) });
@@ -185,6 +199,11 @@ export async function handleQuestion(question, answer, eventRef) {
   }
 }
 
+/** Update the emoji rating data for a particular question response on an event
+ * @param {string} question - the name/label of the question
+ * @param {string} answer - array [int]
+ * @param {Object} eventRef - a Firebase doc ref object
+ */
 export async function handleEmojiQuestion(question, index, eventDoc, eventRef) {
   try {
     const data = await eventDoc.data();
@@ -192,9 +211,7 @@ export async function handleEmojiQuestion(question, index, eventDoc, eventRef) {
             RATHER AN OBJECT!!!!!
             WILL ONLY WORK HARD CODED, data.question does not work
             */
-    const originalArray = Object.keys(data[question]).map(
-      (key) => data[question][key],
-    ); //grab array from firestore
+    const originalArray = Object.keys(data[question]).map((key) => data[question][key]); //grab array from firestore
     const copiedArray = [...originalArray]; //copy the array locally
     copiedArray[index] += 1; //increment that spit
 
@@ -210,6 +227,12 @@ export async function handleEmojiQuestion(question, index, eventDoc, eventRef) {
   }
 }
 
+/** Store survey response data in the Firebase database
+ * @param {string} question - the question being responded to
+ * @param {string} answer - the user provided answer to the question
+ * @param {string} uid - the Firebase user id
+ * @param {string} eventName - the name of the associated event
+ */
 export async function sendFeedbackToDB(question, answer, uid, eventName) {
   try {
     const emojis = new EmojiQuestions();
